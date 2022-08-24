d4a9a5988453c03fe595850949c58faa423f113b
nrcmouli <32757545+nrcmouli@users.noreply.github.com>
2022-08-24 10:09:25 -0700

    Create number_of_args.py

---
+++ number_of_args.py
@@ -0,0 +1,15 @@
+
+def no_of_argu(*args):
+     
+    # using len() method in args to count
+    return(len(args))
+ 
+ 
+a = 1
+b = 3
+ 
+# arguments passed
+n = no_of_argu(1, 2, 4, a)
+ 
+# result printed
+print(" The number of arguments are: ", n)
